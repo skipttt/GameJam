78b59ba3e0c6c13191baa4559ce92c5a4dbbe1b1
José Isai Galvan Villegas <160529629+Isai2222@users.noreply.github.com>
2025-05-10 20:22:02 -0600

    Menus
    Main Menu y Pause Menu

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeSelf)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
